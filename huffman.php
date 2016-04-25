d722a9b70608c99b9e9a607286097696da0441a0
dbseller <renanrmelo@gmail.com>
2016-04-25 13:15:46 -0300

    Continuando

--- huffman.php
+++ huffman.php
@@ -8,59 +8,34 @@ $string = 'bom esse bombom';
 /**
  * Calculamos a probabilidade de cada um dos caracteres
  */
-$aProbability = array();
+$aTree = array();
 
 foreach (count_chars($string, 1) as $asc => $count ) {
-  $aProbability[chr($asc)]  = $count;
+  $aTree[]  = array($count, chr($asc));
 }
-
 
 /**
  * Ordenamos o array de forma crescente.
  */
 asort($aProbability);
 
-$aTree = array();
-
-
-
-foreach ($aProbability as $char => $key) {
-  $aTree[][$key] = $char;
-}
-
 while (count($aTree) > 1) {
   $aTree = doThehand($aTree);
 }
 
-
-
 function doTheHand($aTree) {
 
+  $t1 = array_shift($aTree);
+  $t2 = array_shift($aTree);
 
-  $t1 = $aTree[0];
-  $t2 = $aTree[1];
-
-  $weight = (int) array_keys($t1)[0] + array_keys($t2)[0];
-  
-  
-
-  $aAux = array();
-
-  $aAux[0] =  $t1;
-  $aAux[1] =  $t2;
-
-  $r = array();
-
-  $r[$weight] = $aAux;
-
-  unset($aTree[0]);
-  unset($aTree[1]);
-
-  $aTree[] = $r;
+  $aTree[] = array($t1[0] + $t2[0], array($t1, $t2));
 
   sort($aTree);
-
   return $aTree;
 }
 
 print_r($aTree);
+
+
+echo "=======================" . PHP_EOL;
+print_r(array_search("b", $aTree));
